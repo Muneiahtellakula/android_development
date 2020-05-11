7f6a993831fff10748b9ef278b7787bcadd4a1f0
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-05-11 19:20:23 +0530

    initial commit

---
+++ Android2020/app/src/main/java/com/muneiah/android2020/HelloToast/Hello_Toast_Class.java
@@ -0,0 +1,30 @@
+package com.muneiah.android2020.HelloToast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.muneiah.android2020.R;
+
+public class Hello_Toast_Class extends AppCompatActivity {
+TextView label;
+int c=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_toast);
+        label=findViewById(R.id.tv_count);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "Hello there,Welcome ..to our First app :"+label.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void showCount(View view) {
+        c++;
+        label.setText(""+c);
+    }
+}
---
+++ Android2020/app/src/main/java/com/muneiah/android2020/ui/home/HomeFragment.java
@@ -0,0 +1,53 @@
+package com.muneiah.android2020.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.muneiah.android2020.HelloToast.Hello_Toast_Class;
+import com.muneiah.android2020.MainActivity;
+import com.muneiah.android2020.R;
+import com.muneiah.android2020.text_and_scrollView.TextAndScrollViewActivity;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final Button toastButton = root.findViewById(R.id.hello_toastApp_btn);
+        final Button scrollViewButton = root.findViewById(R.id.text_sroll_btn);
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(), Hello_Toast_Class.class);
+                startActivity(intent);
+            }
+        });
+        scrollViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), TextAndScrollViewActivity.class));
+            }
+        });
+       /* homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        return root;
+    }
+}
