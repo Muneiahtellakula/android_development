e206ff9f77d164096cc34799eb3968c13de98ddc
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-05-15 14:40:08 +0530

    recyclerview

---
+++ Android2020/app/src/main/java/com/muneiah/android2020/RecyclerView/RecyclerView.java
@@ -0,0 +1,16 @@
+package com.muneiah.android2020.RecyclerView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.muneiah.android2020.R;
+
+public class RecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}
