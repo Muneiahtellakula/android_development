ca2cd3a1354b4796c0a3e069e331253b5566ea47
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-05-28 11:09:37 +0530

    room live db

---
+++ Android2020/app/src/main/java/com/muneiah/android2020/RoomDataBase/RoomDataBaseActivity.java
@@ -0,0 +1,16 @@
+package com.muneiah.android2020.RoomDataBase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.muneiah.android2020.R;
+
+public class RoomDataBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_data_base);
+    }
+}
